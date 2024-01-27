8689a6084a92722e7278a490035202820cdadeee
Ade3164 <Adebayoidris051@gmail.com>
2024-01-27 09:36:43 +0100

    directory update

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 // Root API
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Welcome to ivsync - Your Health Partner" });
 });
 
