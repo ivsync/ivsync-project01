a8b74526db008ccff1c18afcb62f9c37647acb89
Ade3164 <Adebayoidris051@gmail.com>
2024-02-07 17:16:04 +0100

    updated

--- src/models/device.js
+++ src/models/device.js
@@ -1,12 +1,16 @@
+// Import mongoose
 const mongoose = require("mongoose");
 
-const deviceSchema = new mongoose.Schema({
-  device_id: { type: String, required: true, unique: true },
-  battery: { type: Number, min: 0, max: 100, default: 100 },
-  owner_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  status: { type: String, required: true },
-}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
+// Define Device Schema
+const deviceSchema = new mongoose.Schema(
+  {
+    device_id: { type: String, required: true, unique: true },
+    battery: { type: Number, min: 0, max: 100, default: 100 },
+    owner_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    status: { type: String, required: true },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
 
-const Device = mongoose.model("device", deviceSchema);
-
-module.exports = Device;
+// Create and export Device model
+module.exports = mongoose.model("Device", deviceSchema);
