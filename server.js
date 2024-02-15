c89c0f0f2f46b63594faaa96266796b58c8b1edd
Ade3164 <Adebayoidris051@gmail.com>
2024-02-15 07:24:49 +0100

    server updated allow cross origins

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-// app.js
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./src/db");
-const deviceRouter = require('./src/controllers/device');
-
+const deviceRouter = require("./src/controllers/device");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(bodyParser.json());
-
 
 // Middleware for JSON responses
 app.use((req, res, next) => {
@@ -24,10 +20,18 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to ivsync - Your Health Partner" });
 });
 
+// Allow CORS for both HTTP and HTTPS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-app.use('/api', deviceRouter);
+// Mount deviceRouter for API routes
+app.use("/api", deviceRouter);
 
-
+// Connect to the database and start the server
 app.listen(port, async () => {
   await db.connect();
   console.log(`Server is running on port ${port}`);
